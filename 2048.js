9d38d0d233af4b54a3517db64aedcb41a468f57e
Sunmeet2000 <44989181+Sunmeet2000@users.noreply.github.com>
2022-02-06 11:40:27 +0530

    Update 2048.js

--- 2048.js
+++ 2048.js
@@ -1,2 +1,18 @@
 document.addEventListener('DOMContentLoaded',() =>{
+    const boardDisplay = document.querySelector('.board')
+
+    let sqs = []
+    let score = 0
+
+    function board()
+    {
+        for(let i=0;i < 16;i++)
+        {
+            sq = document.createElement('div')
+            square.innerHTML = 0
+            boardDisplay.appendChild(sq)
+            sqs.push(sq)
+        }
+    }
+    board()
 })
