464245ec6fe18cc5a8a930083c361de02de38406
Sunmeet2000 <44989181+Sunmeet2000@users.noreply.github.com>
2022-02-06 15:21:51 +0530

    Update 2048.js

--- 2048.js
+++ 2048.js
@@ -1,30 +1,48 @@
 document.addEventListener('DOMContentLoaded',() =>{
     const boardDisplay = document.querySelector('.board')
+    const scoreDisplay =  document.getElementById('score')
 
     let sqs = []
+    let score = 0
+    let arr = [2,4]
+    
 
+    //Creating Board
     function board()
     {
-        for(let i=0;i < 16;i++)
+        for(let i=0;i<4;i++)
         {
-            sq = document.createElement('div')
-            sq.innerHTML = ""
-            boardDisplay.appendChild(sq)
-            sqs.push(sq)
+            sqs[i] = []
+        }
+        for(let i=0;i < 4;i++)
+        {
+            for(let j=0;j<4;j++)
+            {
+                sq = document.createElement('div')
+                sq.innerHTML = 0
+                boardDisplay.appendChild(sq)
+                sqs[i].push(sq)
+            }
         }
         generate24()
     }
     board()
 
-    //Inserting 2 & 4 at random position
+
+    //Inserting 2 & 4 at random positions initially.
     function generate24()
     {
         let num1 = Math.floor(Math.random() * 16)
         let num2 = Math.floor(Math.random() * 16)
-        if(num1 != num2 && sqs[num1].innerHTML == "" && sqs[num2].innerHTML == "")
+        let i1,j1,i2,j2
+        i1 = Math.floor(num1 / 4) 
+        j1 = (num1 % 4)
+        i2 = Math.floor(num2 / 4)
+        j2 = (num2 % 4)
+        if(num1 != num2 && sqs[i1][j1].innerHTML == 0 && sqs[i2][j2].innerHTML == 0)
         {
-            sqs[num1].innerHTML = 2
-            sqs[num2].innerHTML = 4
+            sqs[i1][j1].innerHTML = 2
+            sqs[i2][j2].innerHTML = 4
             //checkForGameOver()
         }
         else
@@ -32,32 +50,68 @@ document.addEventListener('DOMContentLoaded',() =>{
     }
 
 
+    //Inserting 2 or 4 at random position.
+    function generate()
+    {
+        let num1 = Math.floor(Math.random() * 16)
+        let i,j
+        i = Math.floor(num1 / 4)
+        j = (num1 % 4)
+        if(sqs[i][j].innerHTML == 0)
+        {
+            let num2 = Math.floor(Math.random() * 2)
+            sqs[i][j].innerHTML = arr[num2]
+        }
+        else
+          generate()
+    }
+
+
     //Moving Right
     function rightmove()
     {
-        for(let i=0;i<16;i++)
+        for(let i=0;i<4;i++)
         {
-            if(i % 4 === 0)
-            {
-                let One = sqs[i].innerHTML
-                let Two = sqs[i+1].innerHTML
-                let Three = sqs[i+2].innerHTML
-                let Four = sqs[i+3].innerHTML
+                let a = sqs[i][0].innerHTML
+                let b = sqs[i][1].innerHTML
+                let c = sqs[i][2].innerHTML
+                let d = sqs[i][3].innerHTML
 
-                let row = [parseInt(One),parseInt(Two),parseInt(Three),parseInt(Four)]
+                let row = [parseInt(a),parseInt(b),parseInt(c),parseInt(d)]
              
-                let filteredRow = row.filter(num => num)
-                let missing = 4 - filteredRow.length
-                let zeros = Array(missing).fill(0)
-                let newRow = zeros.concat(filteredRow)
+                let filterRow = row.filter(num => num)
+                let miss = 4 - filterRow.length
+                let zeros = Array(miss).fill(0)
+                let newRow = zeros.concat(filterRow)
 
-                sqs[i].innerHTML = newRow[0]
-                sqs[i+1].innerHTML = newRow[1]
-                sqs[i+2].innerHTML = newRow[2]
-                sqs[i+3].innerHTML = newRow[3]
-            }
+                sqs[i][0].innerHTML = newRow[0]
+                sqs[i][1].innerHTML = newRow[1]
+                sqs[i][2].innerHTML = newRow[2]
+                sqs[i][3].innerHTML = newRow[3]
         }      
     }
+
+    
+
+    function combinerow()
+    {
+        for(let i=0;i<4;i++)
+        {
+            for(let j=0;j<3;j++)
+            {
+                if(sqs[i][j].innerHTML === sqs[i][j+1].innerHTML)
+                {
+                    let combinedTotal = parseInt(sqs[i][j].innerHTML) + parseInt(sqs[i][j+1].innerHTML)
+                    sqs[i][j].innerHTML = combinedTotal
+                    sqs[i][j+1].innerHTML = 0
+                    score += combinedTotal
+                    scoreDisplay.innerHTML = score
+                }
+            }
+        }
+       // checkForWin()
+    }
+
 
 
     function control(e)
@@ -67,6 +121,13 @@ document.addEventListener('DOMContentLoaded',() =>{
             keyRight()
         }
 
+    }
+    function keyRight()
+    {
+        rightmove()
+        combinerow()
+        rightmove()
+        generate()      
     }
 
 })
