102ea5b6bbc51d57f0286fca6b7b5744090c65c3
Sunmeet2000 <44989181+Sunmeet2000@users.noreply.github.com>
2022-02-06 12:41:23 +0530

    Update 2048.js

--- 2048.js
+++ 2048.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded',() =>{
     const boardDisplay = document.querySelector('.board')
 
     let sqs = []
-
+    
+    //Generating Board
     function board()
     {
         for(let i=0;i < 16;i++)
@@ -12,6 +13,25 @@ document.addEventListener('DOMContentLoaded',() =>{
             boardDisplay.appendChild(sq)
             sqs.push(sq)
         }
+        generate24()
     }
     board()
+
+    //Inserting 2 & 4 at random positions
+    function generate24()
+    { 
+        let num1
+        let num2
+        while(1)
+        {
+            num1 = Math.floor(Math.random() * sqs.length)
+            num2 = Math.floor(Math.random() * sqs.length)
+            if(num1 != num2 && sqs[num1].innerHTML == 0 && sqs[num2].innerHTML == 0)
+               break
+        }
+
+        sqs[num1].innerHTML = 2
+        sqs[num2].innerHTML = 4
+        //checkForGameOver()
+    }
 })
