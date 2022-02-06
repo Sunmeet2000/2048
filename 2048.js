1b71fe4107d11fd5d32ddc80564aaa621c00b6c9
Sunmeet2000 <44989181+Sunmeet2000@users.noreply.github.com>
2022-02-06 11:57:32 +0530

    Update 2048.js

--- 2048.js
+++ 2048.js
@@ -2,14 +2,13 @@ document.addEventListener('DOMContentLoaded',() =>{
     const boardDisplay = document.querySelector('.board')
 
     let sqs = []
-    let score = 0
 
     function board()
     {
         for(let i=0;i < 16;i++)
         {
             sq = document.createElement('div')
-            square.innerHTML = 0
+            sq.innerHTML = 0
             boardDisplay.appendChild(sq)
             sqs.push(sq)
         }
